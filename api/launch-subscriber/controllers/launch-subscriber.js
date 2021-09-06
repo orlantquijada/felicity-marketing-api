8fa00ce5106eb81011c0d6719d26d080f478d04d
Orlan Quijada <orlantquijada@gmail.com>
2021-09-06 11:45:18 +0800

    get or create notify me endpoint

--- api/launch-subscriber/controllers/launch-subscriber.js
+++ api/launch-subscriber/controllers/launch-subscriber.js
@@ -1,8 +1,37 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    const data = ctx.is("multipart")
+      ? parseMultipartData(ctx).data
+      : ctx.request.body;
+
+    let entity = await strapi.services["launch-subscriber"].findOne({
+      email: data.email,
+    });
+
+    if (!entity)
+      entity = await strapi.services["launch-subscriber"].create(
+        ctx.request.body
+      );
+
+    if (process.env.NODE_ENV === "production")
+      await strapi.plugins["launch-subscriber"].services.email.send({
+        to: entity.email,
+        from: "felicityincorporated@gmail.com",
+        subject: "new subject",
+        text: "message",
+      });
+
+    return sanitizeEntity(entity, {
+      model: strapi.models["launch-subscriber"],
+    });
+  },
+};
