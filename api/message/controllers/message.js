4264a17ceb3e08be4b05ffd6e9661f871843eddb
Orlan Quijada <orlantquijada@gmail.com>
2021-09-06 18:50:12 +0800

    disabled email send on message create - check #4

--- api/message/controllers/message.js
+++ api/message/controllers/message.js
@@ -15,20 +15,18 @@ module.exports = {
 
     const entity = await strapi.services.message.create(data);
 
-    if (process.env.NODE_ENV === "production") {
-      try {
-        await strapi.plugins.email.services.email.send({
-          to: process.env.DEFAULT_EMAIL,
-          from: entity.email,
+    // IMPORTANT: currently not possible to send emails from any email ad since it requires domain authentication
+    // domain authentication can only be achieved on a custom doman/subdomain where you can edit DNS records
+    // not possible because domain is currently not configured yet (currently using heroku's default free domain)
+    // if (process.env.NODE_ENV === "production")
+    //     await strapi.plugins.email.services.email.send({
+    //       to: process.env.DEFAULT_EMAIL,
+    //       from: entity.email,
 
-          // subject to change
-          subject: "new subject",
-          text: "message",
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
+    //       // subject to change
+    //       subject: "new subject",
+    //       text: "message",
+    //     });
 
     return sanitizeEntity(entity, { model: strapi.models.message });
   },
