dce69599c4e5d706e695c31c10d599383a6cb899
Orlan Quijada <orlantquijada@gmail.com>
2021-09-06 18:26:00 +0800

    message email logging

--- api/message/controllers/message.js
+++ api/message/controllers/message.js
@@ -15,15 +15,20 @@ module.exports = {
 
     const entity = await strapi.services.message.create(data);
 
-    if (process.env.NODE_ENV === "production")
-      await strapi.plugins.email.services.email.send({
-        to: process.env.DEFAULT_EMAIL,
-        from: entity.email,
+    if (process.env.NODE_ENV === "production") {
+      try {
+        await strapi.plugins.email.services.email.send({
+          to: process.env.DEFAULT_EMAIL,
+          from: entity.email,
 
-        // subject to change
-        subject: "new subject",
-        text: "message",
-      });
+          // subject to change
+          subject: "new subject",
+          text: "message",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
     return sanitizeEntity(entity, { model: strapi.models.message });
   },
